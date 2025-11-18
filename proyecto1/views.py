0620abc207808b1ec012997d04dffa37611a051c
ValidacionesVCC <Gerencia@validacionesvcc.com>
2025-11-18 03:04:14 -0500

    Delete proyecto1/views.py

--- proyecto1/views.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from core import views as core_views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', core_views.inicio, name='inicio'),
-    path('accounts/', include('accounts.urls')),
-]
