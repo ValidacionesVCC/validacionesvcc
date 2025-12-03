feb861188ca94ef14004914c1725ee4395bb0e74
ValidacionesVCC <Gerencia@validacionesvcc.com>
2025-12-02 22:48:33 -0500

    Remove unnecessary whitespace in urls.py

--- proyecto1/urls.py
+++ proyecto1/urls.py
@@ -3,10 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # ruta de tu página principal
     path('', include('core.urls')),
-
-    # rutas de login / register / logout
     path('app/', include('accounts.urls')),
 ]
