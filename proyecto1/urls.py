dd6bc4f02ca5ad695197da1b6c888fbdfd4473cd
ValidacionesVCC <Gerencia@validacionesvcc.com>
2025-12-03 01:23:17 -0500

    Update urls.py

--- proyecto1/urls.py
+++ proyecto1/urls.py
@@ -1,8 +1,19 @@
+# proyecto1/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
+from core.views import convertir_pdf_en_imagenes_pdf
 
 urlpatterns = [
+    # Panel admin
     path('admin/', admin.site.urls),
+
+    # Rutas públicas (inicio, etc.)
     path('', include('core.urls')),
+
+    # Rutas de la app de cuentas (login, registro, etc.)
     path('app/', include('accounts.urls')),
+
+    # API para convertir PDF a PDF con imágenes (Power Automate)
+    path('api/convertir_pdf/', convertir_pdf_en_imagenes_pdf, name='convertir_pdf'),
 ]
