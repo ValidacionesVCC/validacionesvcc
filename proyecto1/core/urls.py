2737389b259750447be0a37aa243edd274edf765
ValidacionesVCC <Gerencia@validacionesvcc.com>
2025-12-03 01:43:16 -0500

    Update urls.py

--- proyecto1/core/urls.py
+++ proyecto1/core/urls.py
@@ -1,1 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from core.views import convertir_pdf_en_imagenes_pdf
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # Página principal
+    path('', include('core.urls')),
+
+    # Login, register, logout
+    path('app/', include('accounts.urls')),
+
+    # API PDF → PDF imágenes
+    path('api/convertir_pdf/', convertir_pdf_en_imagenes_pdf, name='convertir_pdf'),
+]
 
